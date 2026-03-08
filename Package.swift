5a33d51689fbe3b2e6731875a490c7089f657f94
Philip Daquin <philip@daquin.com>
2026-03-08 13:12:06 +0000

    Fix Package.swift and build issues

--- Package.swift
+++ Package.swift
@@ -1,3 +1,12 @@
 // swift-tools-version: 5.9
 import PackageDescription
-let package = Package(name: "saas-cli", version: "1.0.0", description: "Build SaaS apps in seconds", license: .mit, authors: [.init(name: "Philip Daquin", email: "philip@daquin.com")], targets: [.executableTarget(name: "saas-cli", dependencies: [], path: "Sources")])
+
+let package = Package(
+    name: "saas-cli",
+    targets: [
+        .executableTarget(
+            name: "saas-cli",
+            path: "Sources"
+        )
+    ]
+)
